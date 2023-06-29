eef186fe427198532decb3705030f402777d2205
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-06-29 13:26:07 +0300

    The copyright block was developed. Added motion animation

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from 'react'
+
+// import motion
+import { motion } from 'framer-motion'
 
 const Copyright = () => {
-  return <div>Copyright</div>;
-};
+  return (
+    <div className='bg-dark text-[#dbdbdb] py-6 border-t border-[#2b2b2b]'>
+      <div className="container mx-auto">
+        <motion.div
+          initial={{y: 40, opacity: 0}}
+          whileInView={{y: 0, opacity: 1}}
+          transition={{
+            type: 'tween',
+            duration: 0.8,
+            delay: 0.2,
+          }}
+          viewport={{once: false, amount: 0}}
+          className='tracking=[0.02em] text-base'
+        >
+          &copy; 2023 
+          <span className='font-semibold text-white'>The Void Tatto.</span>
+          All rights reserved.
+        </motion.div>
+      </div>
+    </div>
+  )
+}
 
-export default Copyright;
+export default Copyright
